a3966ed3d308ea73cecf351e4ef023b3ed28ad57
Karan Patel <61610858+Karanj1199@users.noreply.github.com>
2022-12-13 21:00:08 -0500

    Final Commit

--- DATABASE FILE/ecommerceweb.sql
+++ DATABASE FILE/ecommerceweb.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 20, 2022 at 07:39 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
